5a5c1fbcbe97ee002f465f99debfc831873b4cb6
Jenil Thakker <jenilkaushik.thakker@sjsu.edu>
2019-07-23 13:47:36 -0700

    Update cert.sol

--- cert.sol
+++ cert.sol
@@ -13,6 +13,9 @@ contract Certificate {
 
         // date when the certificate was assigned.
         uint issue_date;
+        
+        // data stored in the IoT device.
+        string data;
     }
     
     iotCert[] public iotCertificates;
@@ -21,30 +24,40 @@ contract Certificate {
     * @dev Constructor sets the original `owner` of the contract to the sender account, and
     * commits the certificate details to the blockchain
     */
-    function issueCert(uint id, address _owner, string _RFID, uint _DeviceID, string _firmwareVersion, uint _issue_date) public {
+    function issueCert(uint id, address _owner, string _RFID, uint _DeviceID, 
+    string _firmwareVersion, uint _issue_date, string _data) public {
+        
         // Setting the amount of certificates for testing.
         iotCertificates.length = 100;
         
+        // 
         iotCertificates[id].owner = _owner;
         iotCertificates[id].RFID = _RFID;
         iotCertificates[id].DeviceID = _DeviceID;
         iotCertificates[id].firmwareVersion = _firmwareVersion;
         iotCertificates[id].issue_date = _issue_date;
-
+        iotCertificates[id].data = _data;
     }
 
     /**
     * @dev returns contract metadata in one function call, rather than separate .call()s
     */
-    function getCertificateDetails(uint id) public view returns (
-        address, string, uint, string, uint) {
-        return (
+    function verifyCertificateDetails(uint id, address _owner, uint _DeviceID) public returns (
+        address, string, uint, string, uint, string) {
+        // Verifying owner's identity and device number to grant 
+        // access to the data.
+        require(_owner == iotCertificates[id].owner 
+        && _DeviceID == iotCertificates[id].DeviceID);
+        
+            return (
             iotCertificates[id].owner,
             iotCertificates[id].RFID,
             iotCertificates[id].DeviceID,
             iotCertificates[id].firmwareVersion,
-            iotCertificates[id].issue_date
+            iotCertificates[id].issue_date,
+            iotCertificates[id].data
             );
+        
     }
     
 }
